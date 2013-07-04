e6f770e811a75200c644499fa784d727f0d3286c
roylines <roy@roylines.co.uk>
2013-07-04 21:31:37 +0100

    added redis

--- plans/clipper/default.pp
+++ plans/clipper/default.pp
@@ -1,10 +1,14 @@
-class base {
-	package { "ntp":
-	  ensure  => installed
+class firewall {
+	file { "/etc/sysconfig/iptables":
+		source => "/plans/iptables"
 	}
-	package { "git":
-	  ensure  => installed
-	}
+	exec { "fw-restart":								
+		command => "/sbin/service iptables restart",
+		subscribe => File["/etc/sysconfig/iptables"],
+		refreshonly => true,
+		user => root,
+		require => File["/etc/sysconfig/iptables"]
+	}	
 }
 
 class nginx {
@@ -47,6 +51,80 @@ class nginx {
 	}
 }
 
+class redis {
+	package { "make":
+		ensure => installed
+	}
+	package { "gcc":
+		ensure => installed
+	}
+	file { "/etc/redis":
+		ensure => directory
+	}
+	file { "/etc/redis/6379.conf":
+		source => "/plans/redis.conf",
+		require => File['/etc/redis']
+	}
+	file { "/etc/init.d/redis_6379":
+		mode	=> 700,
+		source => "/plans/redis.init.d"
+	}
+	file { "/var/redis":
+		ensure => directory
+	}
+	file { "/var/redis/6379":
+		ensure => directory,
+		require => File['/var/redis']
+	}
+	exec { "get-redis":								
+		command => "/usr/bin/wget http://redis.googlecode.com/files/redis-2.6.14.tar.gz",
+		creates => "/usr/local/src/redis-2.6.14.tar.gz",
+		cwd => "/usr/local/src",
+		user => root
+	}
+	exec { "extract-redis":								
+		command => "/bin/tar xzf redis-2.6.14.tar.gz",
+		creates => "/usr/local/src/redis-2.6.14",
+		cwd => "/usr/local/src",
+		user => root,
+		require => Exec["get-redis"]
+	}
+	exec { "make-redis":								
+		command => "/usr/bin/make distclean all",
+		creates => "/usr/local/src/redis-2.6.14/src/redis-cli",
+		cwd => "/usr/local/src/redis-2.6.14",
+		user => root,
+		require => [Exec["extract-redis"], Package["make"], Package["gcc"]]
+	}
+	file { "/usr/local/bin/redis-server":
+			ensure => link,
+			target => "/usr/local/src/redis-2.6.14/src/redis-server",
+			require => Exec['make-redis']
+	}
+	file { "/usr/local/bin/redis-cli":
+			ensure => link,
+			target => "/usr/local/src/redis-2.6.14/src/redis-cli",
+			require => Exec['make-redis']
+	}
+	exec { "chkconfig-redis":								
+		command => "/sbin/chkconfig --level 345 redis_6379 on",
+		creates => "/etc/rc3.d/S80redis_6379",
+		user => root,
+		require => File['/etc/init.d/redis_6379']
+	}
+	exec { "start-redis":								
+		command => "/etc/init.d/redis_6379 start",
+		creates => "/var/run/redis_6379.pid",
+		user => root,
+		require => [
+			File['/etc/init.d/redis_6379'], 
+			File['/usr/local/bin/redis-server'], 
+			File['/etc/redis/6379.conf'],
+			File['/var/redis/6379']
+		]
+	}
+}
+
 class nodejs {
 	exec { "get-nave":								
 		command => "/usr/bin/wget https://raw.github.com/isaacs/nave/master/nave.sh -O /usr/local/bin/nave",
@@ -65,20 +143,7 @@ class nodejs {
 	}
 }
 
-class firewall {
-	file { "/etc/sysconfig/iptables":
-		source => "/plans/iptables"
-	}
-	exec { "fw-restart":								
-		command => "/sbin/service iptables restart",
-		subscribe => File["/etc/sysconfig/iptables"],
-		refreshonly => true,
-		user => root,
-		require => File["/etc/sysconfig/iptables"]
-	}	
-}
-
-include base
-include nginx
-include nodejs
 include firewall
+include nginx
+include redis
+include nodejs
