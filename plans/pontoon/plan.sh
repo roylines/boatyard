8a8b7671865e342a7abf438c91550c188691ca9a
roylines <roy@roylines.co.uk>
2013-12-16 13:32:38 +0000

    get dokku rather than using droplet

--- plans/pontoon/plan.sh
+++ plans/pontoon/plan.sh
@@ -4,5 +4,12 @@ set -e
 # update repositories
 /usr/bin/apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew update
 /usr/bin/apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew upgrade
-/usr/bin/apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew dist-upgrade
-/usr/bin/do-release-upgrade
+# /usr/bin/apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew dist-upgrade
+# /usr/bin/do-release-upgrade
+
+# install dokku
+cd /plans
+export DOKKU_TAG=v0.2.0
+wget https://raw.github.com/progrium/dokku/v0.2.0/bootstrap.sh -O dokku-bootstrap.sh
+chmod u+x /plans/dokku-bootstrap.sh
+/plans/dokku-bootstrap.sh
